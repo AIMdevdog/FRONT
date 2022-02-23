8047c6b267f31eb62db2da50bfcddf3f64a9c52a
fredkeemhaus <fredkeemhaus@gmail.com>
2022-02-24 02:19:57 +0900

    room image select - done

---
+++ src/components/RoomImageSlider.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+
+const roomImage =
+  "https://aim-image-storage.s3.ap-northeast-2.amazonaws.com/map2.png";
+
+const RoomImageSliderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .slick-slider {
+    width: 300px;
+    /* height: 500px; */
+
+    img {
+      width: 100%;
+      padding: 40px;
+    }
+  }
+
+  .slick-initialized .slick-slide {
+    display: flex !important;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const RoomImageSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <RoomImageSliderContainer>
+      <Slider {...settings}>
+        <div>
+          <img src={roomImage} alt="images" />
+        </div>
+        <div>
+          <img src={roomImage} alt="images" />
+        </div>
+      </Slider>
+    </RoomImageSliderContainer>
+  );
+};
+
+export default RoomImageSlider;
