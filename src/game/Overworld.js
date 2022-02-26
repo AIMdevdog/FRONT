8496546e6dc7c7e8006fa559543ff8c3564ee798
gitddabong <wonjong0701@naver.com>
2022-02-26 15:22:20 +0900

    일부 위치에 콘솔 로그 추가
    track, stream, producersExist

--- src/game/Overworld.js
+++ src/game/Overworld.js
@@ -305,6 +305,7 @@ const Overworld = (data) => {
         track,
         ...params
       }
+      console.log("----------- myTrack : ", track)
     } else {
       myStream = await navigator.mediaDevices.getDisplayMedia(
         displayMediaOptions
@@ -456,7 +457,7 @@ const Overworld = (data) => {
             callback({ id })
   
             // if producers exist, then join room
-            console.log()
+            console.log('############# producersExist : ', producersExist)
             if (producersExist) getProducers()
           })
         } catch (error) {
@@ -604,8 +605,10 @@ const Overworld = (data) => {
       
       // destructure and retrieve the video track from the producer
       const { track } = consumer
-      // console.log('**************', new MediaStream([track]));
-      paintPeerFace(new MediaStream([track]), remoteProducerId, "nickname")
+      const peerStream = new MediaStream([track])
+      console.log("----------- peer's Track : ", track)
+      console.log('**************', peerStream);
+      paintPeerFace(peerStream, remoteProducerId, "nickname")
   
       // document.getElementById(remoteProducerId).srcObject = new MediaStream([track])
   
