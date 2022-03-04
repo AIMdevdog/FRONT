73382043fc0f0833747b2a92a7b217716dd2a7d7
fredkeemhaus <fredkeemhaus@gmail.com>
2022-03-05 02:30:24 +0900

    edit audio params

--- src/game/Overworld.js
+++ src/game/Overworld.js
@@ -21,26 +21,9 @@ var remoteConnection = []; // RTCPeerConnection for the "remote"
 
 // WebRTC SFU (mediasoup)
 let params_audio = {
-  encodings: [
-    {
-      rid: "r0",
-      maxBitrate: 100000,
-      scalabilityMode: "S1T3",
-    },
-    {
-      rid: "r1",
-      maxBitrate: 300000,
-      scalabilityMode: "S1T3",
-    },
-    {
-      rid: "r2",
-      maxBitrate: 900000,
-      scalabilityMode: "S1T3",
-    },
-  ],
-  // https://mediasoup.org/documentation/v3/mediasoup-client/api/#ProducerCodecOptions
   codecOptions: {
-    videoGoogleStartBitrate: 1000,
+    opusStereo: 1,
+    opusDtx: 1,
   },
 };
 let params_video = {
