1f59fc4d831fa719a6a23c1ff8546b620b426b11
fredkeemhaus <fredkeemhaus@gmail.com>
2022-03-05 22:20:31 +0900

    deploy

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/sh
-yarn build
-
-# upload s3
-aws s3 sync ./build s3://aim-front
-
-# invalidation cloudfront
-aws cloudfront create-invalidation --distribution-id ELBRB7MUDYQ7O --path '/*'
